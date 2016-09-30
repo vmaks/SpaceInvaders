45e65ec5e3e21c911502a378ac105768aaa16b71
maks <maks.mephi@gmail.com>
2016-09-30 22:46:04 +0300

    add inline

--- src/point2d.hpp
+++ src/point2d.hpp
@@ -154,7 +154,7 @@ private:
   float m_x = 0.0f, m_y = 0.0f;
 };
 
-std::ostream & operator << (std::ostream & os, Point2D const & obj)
+inline std::ostream & operator << (std::ostream & os, Point2D const & obj)
 {
   os << "Point2D {" << obj.x() << ", " << obj.y() << "}";
   return os;
