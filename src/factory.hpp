35f5cb28f8552fcce405405a75fe6646afe58cb2
maks <maks.mephi@gmail.com>
2016-11-09 21:39:42 +0300

    format code

--- src/factory.hpp
+++ src/factory.hpp
@@ -5,7 +5,8 @@
 class Factory
 {
 public:
-  template<typename T, typename... Args> std::shared_ptr<T> Create(Args && ... args)
+  template<typename T, typename... Args>
+  std::shared_ptr<T> Create(Args && ... args)
   {
     return std::shared_ptr<T>(new T(std::forward<Args>(args)...));
   }
