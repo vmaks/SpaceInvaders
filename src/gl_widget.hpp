a248626c670749c3306e85c13cc6a573965e0c07
maks <maks.mephi@gmail.com>
2016-11-21 22:25:50 +0300

    add ability to generate a transperancy parameter

--- src/gl_widget.hpp
+++ src/gl_widget.hpp
@@ -9,6 +9,8 @@
 
 class MainWindow;
 
+double constexpr PI = 3.14159265;
+
 QT_FORWARD_DECLARE_CLASS(QOpenGLTexture)
 QT_FORWARD_DECLARE_CLASS(QOpenGLShader)
 QT_FORWARD_DECLARE_CLASS(QOpenGLShaderProgram)
@@ -45,6 +47,11 @@ private:
   QTime m_time;
   QColor m_background;
   QSize m_screenSize;
+
+
+  /// m_period is a value between 0.0 and 1.0 .
+  /// It as a period.
+  float m_period = 0.0f;
 
   QOpenGLTexture * m_texture = nullptr;
   TexturedRect * m_texturedRect = nullptr;
